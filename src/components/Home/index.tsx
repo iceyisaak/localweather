6796b56a88a4e8d496f8a06c942f39709fe40f26
iceyisaak <me@iceyisaak.com>
2023-11-16 13:41:07 +0100

    Cleaned up code

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,25 +4,12 @@ import SearchBar from '../SearchBar';
 import WeatherDisplay from '../WeatherDisplay';
 
 import style from './home.module.scss';
-import { getCurrentWeatherByGeolocation, getCurrentWeatherByLocationName } from '../../api/current-weather';
-import { useState } from 'react';
 
 
 const Home = () => {
 
     const [coordinates] = useAtom(coordinatesAtom)
     const [locality] = useAtom(localityAtom)
-    // const [hideSearchBar, setHideSearchBar] = useState(false)
-
-    // console.log('coordinates: ', coordinates)
-    // console.log('locality: ', locality)
-
-    // const { isSuccess: gotWeatherByGeo } = getCurrentWeatherByGeolocation()
-    // const { isSuccess: gotWeatherBySearch } = getCurrentWeatherByLocationName()
-
-    // if (gotWeatherByGeo || gotWeatherBySearch) {
-    //     setHideSearchBar(true)
-    // }
 
 
     return (
